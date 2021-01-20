a8a520dba9d82daa1e8581d543f4cf6c8852e199
Sudeep Prasad <sudeep.rp@gmail.com>
2021-01-20 08:55:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Compute Statistics
+
+Here we compute statistics for a bunch of numbers.
+
+This project uses the
+[catch framework](https://github.com/catchorg/Catch2/blob/master/docs/tutorial.md#top)
+to test functionality.
+
+## The task
+
+The code is not complete and doesn't even compile.
+Have a look at the tests to know our intention.
+Design the return type accordingly.
+You need to design a test for an error condition as well.
+
+Take care not to leave behind any compiler warnings in your solution.
---
+++ stats-test.cpp
@@ -0,0 +1,23 @@
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+
+#include "catch.hpp"
+#include "stats.h"
+
+#include <cmath>
+
+TEST_CASE("reports average, minimum and maximum") {
+    auto computedStats = Statistics::ComputeStatistics({1.5, 8.9, 3.2, 4.5});
+    float epsilon = 0.001;
+    REQUIRE(std::abs(computedStats.average - 4.525) < epsilon);
+    REQUIRE(std::abs(computedStats.max - 8.9) < epsilon);
+    REQUIRE(std::abs(computedStats.min - 1.5) < epsilon);
+}
+
+TEST_CASE("average is NaN for empty array") {
+    auto computedStats = Statistics::ComputeStatistics({});
+    //All fields of computedStats (average, max, min) must be
+    //NAN (not-a-number), as defined in math.h
+    
+    //Design the REQUIRE statement here.
+    //Use http://www.cplusplus.com/reference/cmath/isnan/
+}
