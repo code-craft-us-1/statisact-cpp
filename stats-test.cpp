efee4a0236b2a3d24dfdb8b7323f69ea5a8db85a
Sudeep <sudeep.rp@gmail.com>
2021-01-20 09:26:35 +0530

    add alerts

--- stats-test.cpp
+++ stats-test.cpp
@@ -21,3 +21,16 @@ TEST_CASE("average is NaN for empty array") {
     //Design the REQUIRE statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
 }
+
+TEST_CASE("raises alerts when max is greater than threshold") {
+    EmailAlert emailAlert;
+    LEDAlert ledAlert;
+    std::vector<IAlerter*> alerters = {&emailAlert, &ledAlert};
+    
+    const float maxThreshold = 10.2;
+    StatsAlerter statsAlerter(maxThreshold, alerters);
+    statsAlerter.checkAndAlert({99.8, 34.2, 4.5, 6.7});
+
+    REQUIRE(emailAlert.emailSent);
+    REQUIRE(ledAlert.ledGlows);
+}
