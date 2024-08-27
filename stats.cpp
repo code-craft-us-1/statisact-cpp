954cde0374cdcad6e848fb9f40b4603a2efeff36
Prakash Arjunan <aprakash.arjunan@philips.com>
2024-08-27 15:38:45 +0530

    Assignment-1 Resolved namespace issue

--- stats.cpp
+++ stats.cpp
@@ -10,22 +10,24 @@
 #include <numeric> // For std::reduce
 #include <ranges> // For std::ranges and std::ranges
 
-using namespace Statistics;
+namespace Statistics
+{
+    Stats ComputeStatistics(const std::vector<float>& data) {
+        Stats result;
 
-Stats ComputeStatistics(const std::vector<float>& data) {
-    Stats result;
+        if (data.empty()) {
+            result.average = NAN;
+            result.max = NAN;
+            result.min = NAN;
+            return result;
+        }
 
-    if (data.empty()) {
-        result.average = NAN;
-        result.max = NAN;
-        result.min = NAN;
+        result.max = std::ranges::max(data);
+        result.min = std::ranges::min(data);
+        auto const count = static_cast<float>(data.size());
+        result.average = std::reduce(data.begin(), data.end()) / count;
+
         return result;
     }
-    
-    result.max = std::ranges::max(data);
-    result.min = std::ranges::min(data);
-    auto const count = static_cast<float>(data.size());
-    result.average = std::reduce(data.begin(), data.end()) / count;
-
-    return result;
 }
+
