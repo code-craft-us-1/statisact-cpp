43784f7fb605bf39120ac846e84a4164cd6cc5b5
Prakash Arjunan <aprakash.arjunan@philips.com>
2024-08-27 13:52:20 +0530

    Assignement-1 Upskill C++

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,27 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+#include <algorithm> //For std::max and std::min
+#include <vector>
+#include <cmath>  // For NAN and std::abs
+#include <numeric> // For std::reduce
+#include <ranges> // For std::ranges and std::ranges
+
+using namespace Statistics;
+
+Stats ComputeStatistics(const std::vector<float>& data) {
+    Stats result;
+
+    if (data.empty()) {
+        result.average = NAN;
+        result.max = NAN;
+        result.min = NAN;
+        return result;
+    }
+    
+    result.max = std::ranges::max(data);
+    result.min = std::ranges::min(data);
+    auto const count = static_cast<float>(data.size());
+    result.average = std::reduce(data.begin(), data.end()) / count;
+
+    return result;
 }
--- stats.h
+++ stats.h
@@ -1,7 +1,12 @@
 #include <vector>
 
 namespace Statistics {
-    // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<___>& );
+
+    struct Stats {
+        float average;
+        float max;
+        float min;
+    };
+
+    Stats ComputeStatistics(const std::vector<float>& );
 }
