0d1b4040ba373ba2ab091c0512774ce2cc54480e
Ashank Kumar Singh <Ashank.KumarSingh@philips.com>
2024-08-29 00:18:52 +0530

    Assignment submission

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,27 @@
 #include "stats.h"
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+using namespace Statistics;
+Statistics::Stats Statistics::ComputeStatistics(const std::vector<double>& arr) {
     //Implement statistics here
+  Stats outputStruct;
+  outputStruct.min = (double)INT_MAX;
+  outputStruct.max = (double)INT_MIN;
+  double total = 0.0;
+  for (int i = 0; i < arr.size(); i++)
+  {
+    double var = arr.at(i);
+    total += var;
+    if (var < outputStruct.min)
+    {
+      outputStruct.min = var;
+    }
+    if (var > outputStruct.max)
+    {
+      outputStruct.max = var;
+    }
+  }
+
+  outputStruct.average = (double)total /(double)arr.size();
+  return outputStruct;
 }
+
