2388840de24358577537bf2128d7bd6ee3a4ce1b
poojanekar <pooja.nekar@philips.com>
2024-08-30 15:52:52 +0000

    assignment 1

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,25 @@
 #include "stats.h"
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+using namespace Statistics;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& ip1) {
     //Implement statistics here
+
+    Stats sts;
+    
+    if(ip1.size()==0)
+    {
+        sts.average = NAN;
+        sts.min = NAN;
+        sts.max = NAN;
+    }
+    else
+    {
+        sts.average=((float)std::accumulate(ip1.begin(),ip1.end(),0.0f))/ip1.size();
+        sts.min =* std::min_element(ip1.begin(),ip1.end());
+        sts.max =* std::max_element(ip1.begin(),ip1.end());
+    }
+    return st;
 }
