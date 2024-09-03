e26c02f2fc3f805172217b503afbc9845f041945
TonapePriyanka <104907265+TonapePriyanka@users.noreply.github.com>
2024-09-03 12:52:02 +0530

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -15,7 +15,7 @@ TEST(Statistics, AverageNaNForEmpty) {
     auto computedStats = Statistics::ComputeStatistics({});
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
-    EXPECT_TRUE(std:isnan(computedStats.average));
+    EXPECT_TRUE(std::isnan(computedStats.average));
     //Design the gtest EXPECT statement here.
     //Use http://www.cplusplus.com/reference/cmath/isnan/
 }
