a8e44f2ba514cf5d165385a1c646ba7f614c8a7c
TonapePriyanka <104907265+TonapePriyanka@users.noreply.github.com>
2024-09-03 08:34:45 +0000

    Adding structure

--- stats.h
+++ stats.h
@@ -2,6 +2,11 @@
 
 namespace Statistics {
     // define the Stats structure here. See the tests to infer its properties
-    
-    Stats ComputeStatistics(const std::vector<float>& i);
+    struct Stats
+    {
+        float average;
+        float min;
+        float max;
+    };
+    Stats ComputeStatistics(const std::vector<float>&);
 }
